e279d6d5c716fbd81a5563d5886059dd93ff7d65
JoshSanon <49575884+JoshSanon@users.noreply.github.com>
2020-09-03 21:58:02 +1000

    Added background Image, Footer hidden

--- src/component/layout/Landing.js
+++ src/component/layout/Landing.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import robohand from '../../img/robotHand.png';
+
 
 const Landing = () => {
     return (
-        <div className="landing">
+        <div className="landing fill-window" style={{ background: 'rgba(0,0,0,0.1)', backgroundImage:`url(${robohand})`,backgroundPosition: 'center', backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}>
+            <div className= "filter fill-window" style={{ background: 'rgba(0,0,0,0.7)'}}>
             <div className="dark-overlay landing-inner text-light">
-                <div className="container">
+                <div className="container" >
                     <div className="row">
                         <div className="col-md-12 text-center">
-                            <h1 className="display-3 mb-4">
+                            <h1 className="display-3 mb-4" >
                                 Developer Connector
                             </h1>
                             <p className="lead">
@@ -16,7 +19,7 @@ const Landing = () => {
                                 from other developers
                             </p>
                             <hr />
-                            <a href="register.html" className={"btn btn-lg btn-info mr-2"}>
+                            <a href="register.html" className={"btn btn-lg btn-info mr-2"} >
                                 Sign Up
                             </a>
                             <a href={"Login.html"} className={"btn btn-lg btn-light"}>
@@ -26,6 +29,7 @@ const Landing = () => {
                         </div>
                     </div>
                 </div>
+               </div>
             </div>
         </div>
     );
--- src/component/layout/Navbar.js
+++ src/component/layout/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark mb-4">
+        <nav className="navbar navbar-expand-sm navbar-dark bg-dark ">
             <div className="container">
                 <a className="navbar-brand" href="/">
                     CAE-Project
