49dcd1b792231c1f69bb1959427b677f8e401672
Aman Bhuyan <abhuyan@student.unimelb.edu.au>
2020-09-21 23:04:46 +1000

    add one profile pulling

--- server/routes/api/mongo.js
+++ server/routes/api/mongo.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const { ObjectID } = require('mongodb');
 var mongorouter = express.Router();
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://cae-aman:EOvQly0wRkmob7z8@cluster0.6wjtw.mongodb.net/cae-users?retryWrites=true&w=majority&authSource=admin";
@@ -20,6 +21,24 @@ mongorouter.get('/profiles', function(req, res, next) {
             }
         });
     });
+});
+
+mongorouter.get('/p/:ID', function(req, res, next) {
+  const client = new MongoClient(uri, { useNewUrlParser: true });
+  console.log("Trying to get " + req.params.ID + "from mongo");
+  client.connect(err => {
+      const collection = client.db("cae_users").collection("profiles");
+      collection.find(ObjectID(req.params.ID)).toArray((err, p) => {
+          client.close();
+          if (err) {
+            // if an error happens
+            res.send("Error in GET req.");
+          } else {
+            // if all works
+            res.send(p[0]);
+          }
+      });
+  });
 });
 
 module.exports = mongorouter;
