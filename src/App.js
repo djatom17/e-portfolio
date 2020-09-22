007188999d5b9242cfb3d3e1afc544610918eea5
anjali <anjaliahuja99@yahoo.com>
2020-09-22 22:09:41 +1000

    work on sidebar

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import About from "./component/auth/About";
 import Profile2 from "./component/auth/Profile2";
 import Profile3 from "./component/auth/Profile3";
 import Profile4 from "./component/auth/Profile4";
+import Profile5 from "./component/auth/Profile5";
 
 
 import './App.css';
@@ -33,7 +34,7 @@ class App extends Component
                     <Route exact path="/profile2" component={Profile2} />
                     <Route exact path="/profile3" component={Profile3} />
                     <Route exact path="/profile4" component={Profile4} />
-
+                    <Route exact path="/profile5" component={Profile5} />
                     <Route exact path="/register" component={Register} />
                     <Route exact path="/login" component={Login} />
                     <Route exact path="/about" component={About} />
