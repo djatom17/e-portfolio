08785f64a6eaf37e9f39cdb4fc1bb6165a60ca71
JoshSanon <49575884+JoshSanon@users.noreply.github.com>
2020-09-23 02:17:38 +1000

    profile layout 5 added

--- src/component/auth/Profile5.js
+++ src/component/auth/Profile5.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import {Link} from "react-router-dom";
 // import axios from 'axios';
 import * as ProfileData from '../../api/ProfileData';
-import SideNav, { Toggle, Nav, NavItem, NavIcon, NavText } from '@trendmicro/react-sidenav';
+import { Tabs, Tab, TabPanel, TabList } from 'react-web-tabs';
+import 'react-web-tabs/dist/react-web-tabs.css';
 
 
 
@@ -32,10 +33,8 @@ class Profile5 extends Component
     render()
     {
         return (
+            
             <div className="profile">
-                <div className = "side-tab">
-
-                </div>
                 <div className="container">
                     <div className={"row"}>
                         <div className="col-mid-8 m-auto">
@@ -44,17 +43,44 @@ class Profile5 extends Component
                                 {this.state.profile.subtitle}
                             </p>
                             <div className="container browse-outer">
-                                <div className="container browse-profile-picture">
+                                {/* <div className="container browse-profile-picture">
                                     <img src={this.state.profile.image} aria-hidden alt="description of image"/>
-                                </div>
-                                <div className="container browse-profile-summary">
+                                </div> */}
+                                <Tabs defaultTab="vertical-tab-one" vertical>
+                                    <TabList>
+                                    <div className="container browse-profile-picture">
+                                        <img src={this.state.profile.image} aria-hidden alt="description of image"/>
+                                    </div>
+                                        <Tab tabFor="vertical-tab-one">About Me</Tab>
+                                        <Tab tabFor="vertical-tab-two">Achievements</Tab>
+                                        </TabList>
+                                        <TabPanel tabId="vertical-tab-one" className="testing-inner2">
+                                        <h1 className="display-5 text-lg">
+                                            About Me
+                                        </h1>
+                                        <div className="container browse-profile-summary">
+                                            <p>
+                                                {this.state.profile.about}
+                                            </p>
+                                        </div>
+                                        </TabPanel>
+                                        <TabPanel tabId="vertical-tab-two" className="testing-inner2">
+                                        <h1 className="display-5 text-lg">
+                                            Achievements
+                                        </h1>
+                                        <div className="container browse-profile-summary">
+                                            {ProfileData.getElements(this.state.profile.achievements)}
+                                        </div>
+                                        </TabPanel>
+                                </Tabs>
+                                {/* <div className="container browse-profile-summary">
                                     <h1 className="display-5 browse-name">
                                         Achievements
                                     </h1>
                                     <div className="container browse-profile-summary">
                                         {ProfileData.getElements(this.state.profile.achievements)}
                                     </div>
-                                </div>
+                                </div> */}
                             </div>
                             <p>
 
@@ -72,14 +98,14 @@ class Profile5 extends Component
                             <Link to="/profile" className={"btn btn-lg btn-info mr-2"}>
                                 Message
                             </Link>
-                            <h1 className="display-5 text-lg">
+                            {/* <h1 className="display-5 text-lg">
                                 About Me
                             </h1>
                             <div className="container browse-profile-summary">
                                 <p>
                                     {this.state.profile.about}
                                 </p>
-                            </div>
+                            </div> */}
                             <h1 className="display-5 text-lg">
                                 Social Media Links
                             </h1>
