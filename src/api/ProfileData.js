58911fdfdefa52ce6d6a96e70c38c69e8bb57bf3
Aman Bhuyan <abhuyan@student.unimelb.edu.au>
2020-09-22 12:14:51 +1000

    relocate api endpoint

---
+++ src/api/ProfileData.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import axios from 'axios';  
+
+export function getProfile(profileID, callback) 
+{
+    axios.get('/info/p/' + profileID)
+    .then((res) => {
+        return callback(res.data);
+    });
+};
+
+export function getElements (lst)
+{
+    if(lst)
+    {
+        return lst.map((item, index) =>(
+        <p>{item}</p>
+        ));
+    }
+}
+
+export function getName (profile)
+{
+    if(profile)
+    {
+        return(
+            profile.firstName + ' ' + profile.lastName
+        );
+    }
+}
