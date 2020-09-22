93d69b26d56332b4ee8db9c6ba769eb5f9fbd60c
anjali <anjaliahuja99@yahoo.com>
2020-09-22 18:34:12 +1000

    add tabs to profile 2

--- src/component/auth/Profile2.js
+++ src/component/auth/Profile2.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import {Link} from "react-router-dom";
 // import axios from 'axios';
 import * as ProfileData from '../../api/ProfileData';
+import {Tabs, Tab} from 'react-bootstrap-tabs';
 
 class Profile2 extends Component
 {
@@ -19,8 +20,11 @@ class Profile2 extends Component
     render(){
         return (
             <div className="profile">
+
                 <div className="container">
+
                     <div className="row">
+
                         <div className="col-mid-8 m-auto">
                             <h1 className="display-4 text-center">{ProfileData.getName(this.state.profile)}</h1>
                             <p className="lead text-center">
@@ -29,15 +33,34 @@ class Profile2 extends Component
                             <div className="container browse-outer">
                                 <div className="container browse-profile-picture">
                                     <img src={this.state.profile.image} aria-hidden alt="description of image"/>
+
+                                <Tabs onSelect={(index, label) => console.log(label + ' selected')} className = "testing-tab">
+                                  <Tab label="About Me">
+                                  <h1 className="display-5 text-lg">
+                                      About me
+                                  </h1>
+                                  <div className="container browse-profile-summary testing-inner2">
+                                      <p>
+                                          {this.state.profile.about}
+                                      </p>
+                                  </div>
+                                  </Tab>
+
+                                  <Tab label="Achievements">
+                                  <h1 className="display-5 browse-name ">
+                                      Achievments
+                                  </h1>
+                                  <div className="container browse-profile-summary testing-inner2">
+                                      {ProfileData.getElements(this.state.profile.achievements)}
+                                  </div>
+                                  </Tab>
+
+                                </Tabs>
                                 </div>
-                                <div className="container browse-profile-summary">
-                                    <h1 className="display-5 browse-name">
-                                        Achievments
-                                    </h1>
-                                    <div className="container browse-profile-summary">
-                                        {ProfileData.getElements(this.state.profile.achievements)}
-                                    </div>
-                                </div>
+
+                              <h1 className="display-5 text-lg">
+                                  Social Media Links
+                              </h1>
                             </div>
                             <p>
 
@@ -46,14 +69,8 @@ class Profile2 extends Component
                             <h1 className="display-5 text-lg">
                                 About me
                             </h1>
-                            <div className="container browse-profile-summary">
-                                <p>
-                                    {this.state.profile.about}
-                                </p>
-                            </div>
-                            <h1 className="display-5 text-lg">
-                                Social Media Links
-                            </h1>
+
+
                             <div className="container browse-profile-summary">
                                 <p>
                                     {ProfileData.getElements(this.state.profile.social)}
