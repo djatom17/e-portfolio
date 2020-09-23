88108878b76d48cbb3ce8deec779efafc37c5a8e
Aman Bhuyan <abhuyan@student.unimelb.edu.au>
2020-09-23 18:55:35 +1000

    make prettier

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -1,24 +1,21 @@
-const config = require('config');
-const jwt = require('jsonwebtoken');
+const config = require("config");
+const jwt = require("jsonwebtoken");
 
-function auth(req, res, next)
-{
-  const token = req.header('x-auth-token');
+function auth(req, res, next) {
+  const token = req.header("x-auth-token");
 
   // Check for token
-  if(!token) res.status(401).json({ msg: 'No token, authorisation denied'});
+  if (!token)
+    return res.status(401).json({ msg: "No token, authorisation denied" });
 
-  try
-  {
+  try {
     // Verify token
-    const decoded = jwt.verify(token, config.get('jwtSecret'));
+    const decoded = jwt.verify(token, config.get("jwtSecret"));
     // Add user from payload
     req.user = decoded;
     next();
-  } 
-  catch(e)
-  {
-    res.status(400).json({ msg: 'Token is not valid.' });
+  } catch (e) {
+    res.status(400).json({ msg: "Token is not valid." });
   }
 }
 
