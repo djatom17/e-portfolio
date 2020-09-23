ec2aad17ea2faa9c67f94f9a1e4943ec0338f3b0
Aman Bhuyan <abhuyan@student.unimelb.edu.au>
2020-09-23 18:56:11 +1000

    load user and make prettier

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router , Route } from 'react-router-dom';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Navbar from "./component/layout/Navbar";
 import Landing from "./component/layout/Landing";
@@ -15,31 +15,34 @@ import Profile2 from "./component/auth/Profile2";
 import Profile3 from "./component/auth/Profile3";
 import Profile4 from "./component/auth/Profile4";
 
+import { store } from "./store";
+import { loadUser } from "./actions/authActions";
 
-import './App.css';
+import "./App.css";
 
-class App extends Component
-{
-  render()
-  {
+class App extends Component {
+  componentDidMount() {
+    store.dispatch(loadUser());
+  }
+
+  render() {
     return (
-        <Router>
-            <div className="App">
-                <Navbar />
-                <Route exact path="/" component={Landing} />
-                <div className={"container"}>
-                    <Route exact path="/browse" component={Browse} />
-                    <Route exact path="/profile" component={Profile} />
-                    <Route exact path="/profile2" component={Profile2} />
-                    <Route exact path="/profile3" component={Profile3} />
-                    <Route exact path="/profile4" component={Profile4} />
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className={"container"}>
+            <Route exact path="/browse" component={Browse} />
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/profile2" component={Profile2} />
+            <Route exact path="/profile3" component={Profile3} />
+            <Route exact path="/profile4" component={Profile4} />
 
-                    <Route exact path="/register" component={Register} />
-                    <Route exact path="/login" component={Login} />
-                    <Route exact path="/about" component={About} />
-
-                </div>
-            <Footer />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/about" component={About} />
+          </div>
+          <Footer />
         </div>
       </Router>
     );
