8169450d145e37630e1876ce4763ae8780d1ddad
Aman Bhuyan <abhuyan@student.unimelb.edu.au>
2020-09-23 19:52:03 +1000

    fix email type in field

--- src/component/auth/Login.js
+++ src/component/auth/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, Component } from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { login } from "../../actions/authActions";
@@ -31,7 +31,6 @@ class Login extends Component {
   }
 
   handleChange = (text) => (e) => {
-    // setFormData({ ...formData, [text]: e.target.value });
     this.setState({ [e.target.name]: e.target.value });
   };
 
@@ -60,7 +59,7 @@ class Login extends Component {
               <form onSubmit={this.handleSubmit}>
                 <div className="form-group">
                   <input
-                    type={"text"}
+                    type={"email"}
                     className={"form-control form-control-lg"}
                     placeholder="Email"
                     name={"email"}
@@ -92,24 +91,5 @@ const mapStateToProps = (state) => ({
   isAuthenticated: state.auth.isAuthenticated,
   error: state.error,
 });
-
-//const Login = (props) => {
-//  const [formData, setFormData] = useState({
-//    email: "",
-//    password: "",
-//  });
-//
-//  const { email, password } = formData;
-//
-//  const handleChange = (text) => (e) => {
-//    setFormData({ ...formData, [text]: e.target.value });
-//  };
-//
-//  const handleSubmit = (e) => {
-//    e.preventDefault();
-//    console.log("button clicked");
-//    console.log(email, password);
-//  };
-//};
 
 export default connect(mapStateToProps, { login, clearErrors })(Login);
