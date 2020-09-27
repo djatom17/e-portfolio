1d00dc2ae8bdeeb0670dc1d82277a9714eaf3925
Nicholas Wong <49605022+voltun@users.noreply.github.com>
2020-09-28 01:37:52 +0800

    Profile edits can now be updated into Mongo; Comments
    - Added post in mongo.js to handle profile updates to DB.
    - TBC return updated profile or redirect?
    - TBC implement profile updates to mongo.js or myProfile.js?
    - Commented and documented code in mongo.js and myProfile.js

--- server/routes/api/mongo.js
+++ server/routes/api/mongo.js
@@ -1,22 +1,31 @@
+/**
+ * This file mainly provides backend functions for handling
+ * communications with MongoDB's instance.
+ * 
+ * @file  API for MongoDB interactions
+ * @author Team Ctrl-Alt-Elite
+ * @copyright This material is made available to you by or on behalf
+ * of the University of Melbourne.
+ * @requires express,mongodb,config,auth.js,./models/*
+ * @exports mongorouter,postUpload,fetchProfileByUID
+ */
 var express = require("express");
 const { ObjectID } = require("mongodb");
 var mongorouter = express.Router();
-var assert = require("assert");
-const MongoClient = require("mongodb").MongoClient;
 const uri = require("config").get("mongoURI");
 const auth = require("./auth");
 const Profile = require("../../models/Profile");
 const File = require("../../models/File");
 const UserProfile = require('../../models/UserProfile');
 
-function createNewClient() {
-  return new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-}
-
-// Get all profiles
+/**
+ * Fetches all profile entries stored in "profiles" in MongoDB.
+ * 
+ * Calls the table "profiles" via Mongoose and fetches all the profiles stored.
+ * Then, the profiles are converted into an array of JSON objects, adhering to
+ * Profile.js Schema
+ * Sends back the array of profiles as a response body.
+ */
 mongorouter.get("/profiles", function (req, res, next) {
   //get all profile entries from MongoDB.profiles
   Profile.find()
@@ -29,7 +38,12 @@ mongorouter.get("/profiles", function (req, res, next) {
     });
 });
 
-// Get a specific profile based on ObjectID
+/**
+ * Fetches the profile that corresponds to given ObjectID.
+ * 
+ * Functionally similar to get all profiles, except this fetches only one.
+ * @see mongorouter.get('/profiles') 
+ */
 mongorouter.get("/p/:ID", function (req, res, next) {
   console.log("[Mongoose] Fetching " + req.params.ID + " from mongo.");
   Profile.findOne(ObjectID(req.params.ID))
@@ -45,73 +59,78 @@ mongorouter.get("/p/:ID", function (req, res, next) {
     });
 });
 
-// Update a profile's information
-// TODO: must be authorised to make this change.
-mongorouter.post("/p-update/:ID", function (req, res, next) {
-  //Check if req.body fields are valid
+/**
+ * Connects to the "profiles" table and update changes to a profile.
+ * 
+ * //TODO encodeURIComponent needed? Passing profile ID via URI.
+ * //TODO Returns the updated JSON profile, should it redirect?
+ * 
+ * Authenticates user before posting changes.
+ * Finds the document entry in "profiles" table by profile ID.
+ * Updates the entry by only changing data fields that are modified.
+ * Sends back the full, updated profile via res.
+ * 
+ * @param {String} ID Profile ID must be listed in req.params.ID
+ */
+mongorouter.post("/p-update/:ID", auth, function (req, res, next) {
   //Post updates to mongo
-  //???
-  //Profit
-
-  var elements = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    keySkills: req.body.keySkills,
-    workHistory: req.body.workHistory,
-    linkToProfile: req.body.linkToProfile,
-    image: req.body.image,
-  };
-
-  const client = createNewClient();
-  console.log("Trying to send new information to mongo" + elements);
-  client.connect((err) => {
-    assert.strictEqual(null, err);
-    const collection = client.db("cae_users").collection("profiles");
-    collection.updateOne(
-      { _id: ObjectID(req.params.ID) },
-      { $set: elements },
-      function (err, result) {
-        assert.strictEqual(null, err);
-        console.log("item updated");
-        client.close();
-      }
-    );
+  Profile.findOneAndUpdate(
+    {"_id": ObjectID(req.params.ID)},
+    {$set: req.body},
+    {returnNewDocument: true}
+  )
+  .then((err, updated_profile) => {
+    if (err) {
+      console.log("[Mongoose] Error in updating profile.");
+      res.send(null);
+    }
+    console.log("[Mongoose] Successfully posted updates to MongoDB.");
+    res.send(updated_profile);
   });
-
-  // TODO: change this to whatever pages the user was on before making an update
-  res.redirect("/");
 });
 
 // Insert a profile's first-time login information.
 // User exists, but profile itself does not, yet.
 // TODO: must be authorised and logged in.
 mongorouter.post("/p-insert", auth, function (req, res, next) {
-  var profile = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    keySkills: req.body.keySkills,
-    workHistory: req.body.workHistory,
-    linkToProfile: req.body.linkToProfile,
-    image: req.body.image,
-  };
+  // var profile = {
+  //   firstName: req.body.firstName,
+  //   lastName: req.body.lastName,
+  //   keySkills: req.body.keySkills,
+  //   workHistory: req.body.workHistory,
+  //   linkToProfile: req.body.linkToProfile,
+  //   image: req.body.image,
+  // };
 
-  const client = createNewClient();
-  console.log("Trying to send new information to mongo" + profile);
-  client.connect((err) => {
-    assert.strictEqual(null, err);
-    const collection = client.db("cae_users").collection("profiles");
-    collection.insertOne(profile, function (err, result) {
-      assert.strictEqual(null, err);
-      console.log("item inserted");
-      client.close();
-    });
-  });
+  // const client = createNewClient();
+  // console.log("Trying to send new information to mongo" + profile);
+  // client.connect((err) => {
+  //   assert.strictEqual(null, err);
+  //   const collection = client.db("cae_users").collection("profiles");
+  //   collection.insertOne(profile, function (err, result) {
+  //     assert.strictEqual(null, err);
+  //     console.log("item inserted");
+  //     client.close();
+  //   });
+  // });
 
   // TODO: change this to preferably the user's brand-new profile page.
   res.redirect("/");
 });
 
-// Fetches a user's profile data based on user's ID
+/**
+ * Fetches profile JSON data stored in MongoDB.
+ * 
+ * ASSUMES USER ALREADY AUTHENTICATED
+ * Firstly, finds a mapping of uid to pid from table users_to_profiles.
+ * Then, fetches profile data from pid found.
+ * 
+ * @see profilerouter.get in myProfile.js
+ * 
+ * @param {String} uid  User ID of requestee in MongoDB
+ * 
+ * @returns {Object} A JSON object adhering to Profile.js Schema
+ */
 const fetchProfileByUID = (uid) => {
   console.log("[Mongoose] Fetching profile of uid "+uid);
   //Find authenticated user's profile from DB
@@ -131,11 +150,22 @@ const fetchProfileByUID = (uid) => {
         console.log("[Mongoose] Successfully fetched user profile.");
         return profile;
     });
-});
-
-}
+  });
+};
 
 // Post-upload from S3, performs match between file, its hash, and uploader ID
+/**
+ * Creates a document entry in "files" table in MongoDB.
+ * 
+ * Triggered after file upload to S3 is successful. Stores the filename,
+ * key and User ID into "files", for convenience of file retrieval/access.
+ * 
+ * @see s3router.post in s3.js
+ * 
+ * @param {String} name User-defined filename
+ * @param {String} url The key for the file stored in S3
+ * @param {String} uid User ID of owner.
+ */
 const postUpload = (name, url, uid) => {
   console.log("[Mongoose] Creating file entry");
   File.create({ name, url, uid })
--- server/routes/api/myProfile.js
+++ server/routes/api/myProfile.js
@@ -1,22 +1,42 @@
+/**
+ * This file mainly provides backend functions for handling
+ * user-specific profile pages with user auth.
+ * 
+ * @file  API for user-authenticated profiles
+ * @author Team Ctrl-Alt-Elite
+ * @copyright This material is made available to you by or on behalf
+ * of the University of Melbourne.
+ * @requires express,auth.js,mongo.js
+ * @exports profilerouter
+ */
 const express = require("express");
 const profilerouter = express.Router();
 const auth = require("./auth");
 const fetchProfileByUID = require('./mongo').fetchProfileByUID;
 
-//GET a dynamic user profile page (After login and auth)
-profilerouter.get("/", auth, (req, res, next) => {
+/**
+ * Handles GET requests for / AND /edit.
+ * 
+ * Validates user auth token prior to fetching,
+ * fetches the requesting user's profile data stored in MongoDB.
+ * The retrieved user profile is a JSON object, adhering to Profile.js Schema
+ */
+profilerouter.get('/(|edit)', auth, (req, res, next) => {
     var profile = fetchProfileByUID(req.user.id);
     res.send(profile);
 });
 
-//GET a current instance of user's profile data on MongoDB 
-profilerouter.get("/edit", auth, (req, res, next) => {
-    return null;
-});
-
-//POST changes to profile data onto MongoDB
+/**
+ * Handles POST request for /edit.
+ * 
+ * Validates user auth token prior to update.
+ * Finalise changes and uploads changes to MongoDB.
+ */
 profilerouter.post("/edit", auth, (req, res, next) => {
-    return null;
+    //TODO Implement profile edit here or mongo.js?
+
+    //Redirects back to my-profile after changes are done.
+    res.redirect('/');
 });
 
 module.exports = profilerouter;
