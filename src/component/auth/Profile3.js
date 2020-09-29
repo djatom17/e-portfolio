3bb1c1c0b5fb3c4c5450a443a1aaa975aaa91286
anjali <anjaliahuja99@yahoo.com>
2020-09-30 02:01:58 +1000

    add upload button

--- src/component/auth/Profile3.js
+++ src/component/auth/Profile3.js
@@ -11,14 +11,16 @@ import {
   Button,
   Divider,
   Tabs,
+  Form,
+  Upload,
 } from "antd";
 import {
   LinkedinOutlined,
   TwitterOutlined,
   GithubOutlined,
-  MailOutlined,
-  CheckOutlined,
+  UploadOutlined,
 } from "@ant-design/icons";
+import SkillManager from "./SkillManager";
 
 const { Paragraph } = Typography;
 const { TextArea } = Input;
@@ -35,28 +37,61 @@ class Profile3 extends Component {
   };
 
   // functions for editing text
-  setText = (str, value) => {
+  setEditableStr = (value, str) => {
     var temp = { ...this.state.profile };
     temp[value] = str;
     this.setState({ profile: temp });
   };
 
+  setEditableStrArr = (property, index, str) => {
+    var temp = { ...this.state.profile };
+    temp[property][index] = str;
+    this.setState({ profile: temp });
+  };
+
+  getElements(lst, property) {
+    if (lst) {
+      return lst.map((item, index) => (
+        <Paragraph
+          className="psize"
+          editable={{
+            onChange: (e) => this.setEditableStrArr(property, index, e),
+          }}
+        >
+          {item}
+        </Paragraph>
+      ));
+    }
+  }
+
   componentDidMount = () => {
     this.setState({ profile: this.props.profile });
   };
 
-  getElements = (lst) => {
+  getElements(lst, property) {
     if (lst) {
       return lst.map((item, index) => (
-        <div className="media">
-          <p>{item}</p>
-        </div>
+        <Paragraph
+          className="psize"
+          editable={{
+            onChange: (e) => this.setEditableStrArr(property, index, e),
+          }}
+        >
+          {item}
+        </Paragraph>
       ));
     }
-  };
+  }
 
   render() {
-    const { about } = this.state;
+    // for upload
+    const normFile = (e) => {
+      console.log("Upload event:", e);
+      if (Array.isArray(e)) {
+        return e;
+      }
+      return e && e.fileList;
+    };
     return (
       <div clasName="container-fluid mx-4">
         {/* row contains: name, curr job */}
@@ -69,7 +104,7 @@ class Profile3 extends Component {
           </h2>
         </Row>
         {/* row contains: pfp, about me, social media icons */}
-        <Row gutter={24} className="ml-5">
+        <Row justify="space-around" gutter={24} className="mx-5">
           <Col>
             {" "}
             <Avatar src={this.state.profile.image} shape="square" size={200} />
@@ -78,7 +113,7 @@ class Profile3 extends Component {
             <h4>A little bit about me...</h4>
             <Paragraph
               editable={{
-                onChange: (str) => this.setText(str, "about"),
+                onChange: (str) => this.setEditableStr("about", str),
                 autoSize: { minRows: 1, maxRows: 5 },
               }}
             >
@@ -106,17 +141,26 @@ class Profile3 extends Component {
         {/* row contains: tabs  */}
         <Row className=" my-4 ml-5">
           <Tabs onChange={callback} type="card">
-            <TabPane tab="Achievements" key="1">
-              Content of Tab Pane 1
-            </TabPane>
+            <TabPane tab="Achievements" key="1"></TabPane>
             <TabPane tab="Skills" key="2">
-              Content of Tab Pane 2
+              <SkillManager />
             </TabPane>
             <TabPane tab="Projects" key="3">
               Content of Tab Pane 3
             </TabPane>
             <TabPane tab="Certificates" key="4">
-              Content of Tab Pane 4
+              <Form>
+                <Form.Item
+                  name="upload"
+                  label="New File"
+                  valuePropName="fileList"
+                  getValueFromEvent={normFile}
+                >
+                  <Upload name="logo" action="/upload.do" listType="picture">
+                    <Button icon={<UploadOutlined />}>Click to upload</Button>
+                  </Upload>
+                </Form.Item>
+              </Form>
             </TabPane>
             <TabPane tab="Contact Details" key="5">
               Content of Tab Pane 5
@@ -127,99 +171,5 @@ class Profile3 extends Component {
     );
   }
 }
-
-//   render() {
-//     return (
-//       <section className="section about-section gray-bg" id="about">
-//         <div className="container">
-//           <div className="row align-items-center flex-row-reverse">
-//             <div className="col-lg-6">
-//               <div className="about-text go-to">
-//                 <h3 className="dark-color">
-//                   {ProfileData.getName(this.state.profile)}'s Flex
-//                 </h3>
-//                 <h6 className="theme-color lead">
-//                   {this.state.profile.subtitle}
-//                 </h6>
-//                 <br />
-//                 {ProfileData.getElements(this.state.profile.achievements)}
-//                 <br />
-
-//                 <div className="row about-list">
-//                   <div className="col-md-6">
-//                     {this.getElements(this.state.profile.shitdone)}
-//                     <div className="media"></div>
-//                   </div>
-//                   <div className="col-md-6">
-//                     <div className="media">
-//                       <label>E-mail</label>
-//                       <p>LuciousFox@wayne.com</p>
-//                     </div>
-//                     <div className="media">
-//                       <label>Contact</label>
-//                       <p>Bat-Signal</p>
-//                     </div>
-//                     <div className="media">
-//                       <label>MySpace</label>
-//                       <p>BillionaireBruce</p>
-//                     </div>
-//                     <div className="media">
-//                       <label>Justice</label>
-//                       <p>Available</p>
-//                     </div>
-//                   </div>
-//                 </div>
-//               </div>
-//             </div>
-//             <div className="col-lg-6">
-//               <div className="about-avatar">
-//                 <img
-//                   src={this.state.profile.image}
-//                   aria-hidden
-//                   alt="description of image"
-//                 />
-//               </div>
-//             </div>
-//           </div>
-//           <br />
-//           <div>
-//             <h3 className="display-5 text-lg">Skills</h3>
-//             <div className="container text-lg-left">
-//               <p></p>
-//             </div>
-//             <h3 className="display-5 text-lg">About me</h3>
-//             <div className="container text-lg-left">
-//               <p>{this.state.profile.about}</p>
-//             </div>
-//             <br />
-//             <h3 className="display-5 text-lg">Social Media Links</h3>
-//             <div className="row">
-//               <div className="col-6 col-lg-3">
-//                 <div className="count-data text-center">
-//                   <h6 className="count h4" data-to="500" data-speed="500">
-//                     Facebook
-//                   </h6>
-//                   <p className="m-0px font-w-600">
-//                     http://facebook/therealbatman
-//                   </p>
-//                 </div>
-//               </div>
-//               <div className="col-6 col-lg-3">
-//                 <div className="count-data text-center">
-//                   <h6 className="count h4" data-to="500" data-speed="500">
-//                     LinkedIn
-//                   </h6>
-//                   <p className="m-0px font-w-600">
-//                     http://LinkedIn/therealbatman
-//                   </p>
-//                 </div>
-//               </div>
-//             </div>
-//           </div>
-//         </div>
-//       </section>
-//     );
-//   }
-// }
 
 export default Profile3;
