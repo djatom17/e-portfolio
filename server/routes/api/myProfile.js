caab5df9500c7d2341288b37ab1d9c556a856cd6
Aman Bhuyan <abhuyan@student.unimelb.edu.au>
2020-09-29 16:58:01 +1000

    make prettier; remove edit

--- server/routes/api/myProfile.js
+++ server/routes/api/myProfile.js
@@ -1,7 +1,7 @@
 /**
  * This file mainly provides backend functions for handling
  * user-specific profile pages with user auth.
- * 
+ *
  * @file  API for user-authenticated profiles
  * @author Team Ctrl-Alt-Elite
  * @copyright This material is made available to you by or on behalf
@@ -12,31 +12,31 @@
 const express = require("express");
 const profilerouter = express.Router();
 const auth = require("./auth");
-const fetchProfileByUID = require('./mongo').fetchProfileByUID;
+const fetchProfileByUID = require("./mongo").fetchProfileByUID;
 
 /**
- * Handles GET requests for / AND /edit.
- * 
+ * Handles GET requests for /.
+ *
  * Validates user auth token prior to fetching,
  * fetches the requesting user's profile data stored in MongoDB.
  * The retrieved user profile is a JSON object, adhering to Profile.js Schema
  */
-profilerouter.get('/(|edit)', auth, (req, res, next) => {
-    var profile = fetchProfileByUID(req.user.id);
-    res.send(profile);
+profilerouter.get("/", auth, (req, res, next) => {
+  var profile = fetchProfileByUID(req.user.id);
+  res.send(profile);
 });
 
 /**
  * Handles POST request for /edit.
- * 
+ *
  * Validates user auth token prior to update.
  * Finalise changes and uploads changes to MongoDB.
  */
 profilerouter.post("/edit", auth, (req, res, next) => {
-    //TODO Implement profile edit here or mongo.js?
+  //TODO Implement profile edit here or mongo.js?
 
-    //Redirects back to my-profile after changes are done.
-    res.redirect('/');
+  //Redirects back to my-profile after changes are done.
+  res.redirect("/");
 });
 
 module.exports = profilerouter;
