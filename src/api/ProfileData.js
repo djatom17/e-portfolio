82e650abbee9de40ff504a5a91159db27fa5e4b1
Aman Bhuyan <abhuyan@student.unimelb.edu.au>
2020-10-01 18:25:08 +1000

    make prettier

--- src/api/ProfileData.js
+++ src/api/ProfileData.js
@@ -11,14 +11,14 @@ import axios from "axios";
 
 /**
  * Handles API calls to fetch data of a specified profile
- * 
+ *
  * FETCHED DATA IS NULL IF PROFILE DOES NOT EXIST.
  * Calls axios to dispatch backend to make GET request to fetch requested
- * profile's data stored in MongoDB. 
- * 
+ * profile's data stored in MongoDB.
+ *
  * @function [getProfile]
  * @see Models.Profile
- * 
+ *
  * @callback Requester~requestCallback
  * @param {string} profileID - The ObjectID String of profile to get.
  * @param {Requester~requestCallback} callback - Handles callback for response.
@@ -31,12 +31,12 @@ export function getProfile(profileID, callback) {
 
 /**
  * Wraps all items in an array with <p /> tags.
- * 
+ *
  * Maps over all elements in array and creates new array of modified items.
- * 
+ *
  * @function [getElements]
  * @param {Object[]} lst - Array of Objects.
- * 
+ *
  * @returns {Object[]} Array of mapped Objects, wrapped in text tag.
  */
 export function getElements(lst) {
@@ -47,14 +47,14 @@ export function getElements(lst) {
 
 /**
  * Gets the full name of a profile in the format "John Smith".
- * 
+ *
  * Calls firstName and lastName in given profile and concatenate.
- * 
+ *
  * @function [getName]
  * @see Models.Profile
- * 
+ *
  * @param {Object} profile - Profile JSON Schema
- * 
+ *
  * @returns {String} Full name (Firstname Lastname).
  */
 export function getName(profile) {
