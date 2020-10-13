5d1357ba1f51251e3e92a9b58cd064ce3a5a9165
Aman Bhuyan <abhuyan@student.unimelb.edu.au>
2020-10-13 20:42:57 +1100

    google auth working; uses redux states

--- server/routes/api/userAuth.js
+++ server/routes/api/userAuth.js
@@ -4,6 +4,10 @@ const bcrypt = require("bcryptjs");
 const config = require("config");
 const jwt = require("jsonwebtoken");
 const auth = require("./auth");
+const { OAuth2Client } = require("google-auth-library");
+const authClient = new OAuth2Client(
+  "84279213789-nla6f8u88716cs7t53iikugg977laq92.apps.googleusercontent.com"
+);
 
 // User Model
 const User = require("../../models/User");
@@ -69,6 +73,60 @@ userrouter.post("/login", (req, res, next) => {
         });
     });
   });
+});
+
+// Uses token provided by a Google login to maintain login state.
+userrouter.post("/google-login", (req, res, next) => {
+  console.log("[AUTH] Attempting login with Google token");
+  const { tokenId } = req.body;
+  authClient
+    .verifyIdToken({
+      idToken: tokenId,
+      audience:
+        "84279213789-nla6f8u88716cs7t53iikugg977laq92.apps.googleusercontent.com",
+    })
+    .then((response) => {
+      const { email_verified, name, email } = response.getPayload();
+      if (email_verified) {
+        User.findOne({ email })
+          .then((user) => {
+            if (!user)
+              return res.status(400).json({
+                msg: "User does not exist.",
+              });
+
+            UserProfile.findOne({ uid: user.id })
+              .lean()
+              .then((userMap) => {
+                jwt.sign(
+                  {
+                    id: user.id,
+                  },
+                  config.get("jwtSecret"),
+                  {
+                    expiresIn: 3600,
+                  },
+                  (err, token) => {
+                    if (err) throw err;
+                    res.json({
+                      token,
+                      user: {
+                        _id: user.id,
+                        name: user.name,
+                        email: user.email,
+                        pid: userMap.pid,
+                      },
+                    });
+                  }
+                );
+              });
+          })
+          .catch((err) => console.log(err));
+      } else {
+        res.status(401).json({ msg: "Email not verified with Google." });
+      }
+    })
+    .catch((err) => console.log(err));
 });
 
 // Registers new user and gives them a token.
---
+++ src/component/auth/GoogleButton.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { GoogleLogin, GoogleLogout } from "react-google-login";
+import axios from "axios";
+import { googleLogin } from "../../actions/authActions";
+
+const CLIENT_ID =
+  "84279213789-nla6f8u88716cs7t53iikugg977laq92.apps.googleusercontent.com";
+
+class GoogleButton extends Component {
+  state = {
+    isGoogleAuth: false,
+    googleAccessToken: "",
+  };
+
+  handleLoginFailure(response) {
+    alert("Failed to log in");
+  }
+
+  handleLoginSuccess(response) {
+    this.props.googleLogin(response);
+  }
+
+  handleLoginSuccess = this.handleLoginSuccess.bind(this);
+
+  render() {
+    return (
+      <GoogleLogin
+        clientId={CLIENT_ID}
+        buttonText="Sign in with Google"
+        onSuccess={this.handleLoginSuccess}
+        onFailure={this.handleLoginFailure}
+        cookiePolicy={"single_host_origin"}
+        responseType="code,token"
+      />
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+  error: state.error,
+});
+
+export default connect(mapStateToProps, { googleLogin })(GoogleButton);
