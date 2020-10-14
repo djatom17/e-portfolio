4e9c1569e23063bf7b08fc5478e3f7e5b5d79c42
JoshSanon <49575884+JoshSanon@users.noreply.github.com>
2020-10-14 20:22:11 +1100

    Frontend test 1 added

--- src/App.test.js
+++ src/App.test.js
@@ -3,13 +3,13 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import { render } from "@testing-library/react";
 import App from "./App";
-import Profile3 from "/component/auth/Profile3";
+import Profile3 from "./component/auth/Profile3";
 import ProfileData from "./api/ProfileData";
-
-// practice test
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
-});
+import AchievementManager from "./component/profileDisplays/AchievementManager";
+import { configure, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { shallow } from "enzyme";
+configure({ adapter: new Adapter() });
 
 describe("Front-end tests", () => {
   // Create mocking functions to be used for rendering.
@@ -38,5 +38,27 @@ describe("Front-end tests", () => {
       </Provider>,
       div
     );
+  });
+});
+
+// testing AchievementManager
+describe("<AchievementManager /> component", () => {
+  it("should render", () => {
+    const achievements = mount(
+      <AchievementManager
+        isMyProfile={true}
+        canEdit={true}
+        data={[
+          "Pro Golf Champion",
+          "Cross Country Cyclist",
+          "Founder of Charity Hackathon",
+        ]}
+        changeList={() => {
+          alert("function called");
+        }}
+      />
+    );
+    const value = achievements.find("Paragraph").at(1).text();
+    expect(value).toEqual("Pro Golf Champion");
   });
 });
