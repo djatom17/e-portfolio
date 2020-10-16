fb618cbe1011d95568845d3f2515b96062a0c531
JoshSanon <49575884+JoshSanon@users.noreply.github.com>
2020-10-16 21:25:25 +1100

    Testing init

--- src/App.test.js
+++ src/App.test.js
@@ -3,11 +3,13 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import { render } from "@testing-library/react";
 import App from "./App";
-import Profile3 from "./component/auth/Profile3";
+import Profile5 from "./component/auth/Profile5";
 import ProfileData from "./api/ProfileData";
 import AchievementManager from "./component/profileDisplays/AchievementManager";
+import EditButton from "./component/profileDisplays/EditButton";
 import { configure, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
+import renderer from "react-test-renderer";
 import { shallow } from "enzyme";
 configure({ adapter: new Adapter() });
 
@@ -62,3 +64,36 @@ describe("<AchievementManager /> component", () => {
     expect(value).toEqual("Pro Golf Champion");
   });
 });
+
+// testing AchievementManager
+describe("<Edit Button /> component", () => {
+  it("should render", () => {
+    const achievements = mount(
+      <EditButton isMyProfile={true} canEdit={true} />
+    );
+    const value = achievements.find("Button");
+    expect(value).toEqual("Pro Golf Champion");
+  });
+});
+
+// describe("Profile Layout 5 ", () => {
+//   it("when there the user does not have authentication", () => {
+//     const prof3 = mount(
+//       <Provider store={store}>
+//         <Profile5 />{" "}
+//       </Provider>
+//     );
+//     prof3.getInstance().setState({ isMyProfile: false });
+//     expect(prof3).toMatchSnapshot();
+//   });
+
+//   it("when there the user has authentication", () => {
+//     const prof3 = mount(
+//       <Provider store={store}>
+//         <Profile5 />{" "}
+//       </Provider>
+//     );
+//     prof3.getInstance().setState({ isMyProfile: true });
+//     expect(prof3).toMatchSnapshot();
+//   });
+// });
