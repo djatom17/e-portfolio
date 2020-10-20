145a75f2ce17f11a12447aeb619d6319e686a547
Aman Bhuyan <abhuyan@student.unimelb.edu.au>
2020-10-20 23:43:52 +1100

    remove warning from test "keys"

--- src/component/profileDisplays/AchievementManager.js
+++ src/component/profileDisplays/AchievementManager.js
@@ -58,7 +58,7 @@ export class AchievementManager extends Component {
                   );
                 }
                 const achievement = (
-                  <Row>
+                  <Row key={item}>
                     <Col flex="auto">
                       <Paragraph key={item}>
                         <span
