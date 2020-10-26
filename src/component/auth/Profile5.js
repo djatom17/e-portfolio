1ab162cd2def2718aa1902aeb8d6b3c2e9bfd731
JoshSanon <49575884+JoshSanon@users.noreply.github.com>
2020-10-26 19:08:41 +1100

    Prof5 colour works

--- src/component/auth/Profile5.js
+++ src/component/auth/Profile5.js
@@ -32,8 +32,8 @@ class Profile5 extends Component {
     editInputIndex: -1,
     editInputValue: "",
     mobileView: false,
-    primaryColor: "coral",
-    secondaryColor: "white",
+    primaryColour: "",
+    secondaryColour: "",
   };
 
   constructor() {
@@ -55,7 +55,11 @@ class Profile5 extends Component {
     window.addEventListener("resize", this.resize.bind(this));
     this.resize();
     //Authorisation check.
-    this.setState({ layout: this.props.profile.layout });
+    this.setState({
+      layout: this.props.profile.layout,
+      primaryColour: this.props.profile.primaryColour,
+      secondaryColour: this.props.profile.secondaryColour,
+    });
     this.props.isAuthenticated &&
     this.props.profile.userid &&
     this.props.user._id &&
@@ -86,10 +90,10 @@ class Profile5 extends Component {
       ));
     }
   }
-  handlePrimColor = (color, str) => {
+  handlePrimColour = (color, str) => {
     str === "Prim"
-      ? this.setState({ primaryColor: color.hex })
-      : this.setState({ secondaryColor: color.hex });
+      ? this.setState({ primaryColour: color.hex })
+      : this.setState({ secondaryColour: color.hex });
   };
 
   displayProfileSeg = () => {
@@ -152,14 +156,16 @@ class Profile5 extends Component {
     const { current } = this.state.tabdisp;
     return (
       <div
-        className="container-fluid ml-n3 mb-3"
-        style={{ backgroundColor: this.state.secondaryColor }}
+        className="container-fluid"
+        style={{
+          backgroundColor: this.state.secondaryColour,
+        }}
       >
-        <Row className="prof5height">
+        <Row className="prof5height ml-n3">
           <Col flex={1}>
             <div
               className="prof5"
-              style={{ backgroundColor: this.state.primaryColor }}
+              style={{ backgroundColor: this.state.primaryColour }}
             >
               <div className="container-fluid prof5-img">
                 <ProfilePicture
@@ -192,7 +198,7 @@ class Profile5 extends Component {
                   onClick={this.handleTabClick}
                   selectedKeys={[current]}
                   mode="vertical"
-                  style={{ backgroundColor: this.state.primaryColor }}
+                  style={{ backgroundColor: this.state.primaryColour }}
                   className="text-center"
                 >
                   <Menu.Item key="about" className="modified-item">
@@ -248,7 +254,7 @@ class Profile5 extends Component {
                     layout={this.state.layout}
                     visible={this.state.visible}
                     loading={this.state.loading}
-                    handlePrimColor={this.handlePrimColor}
+                    handlePrimColour={this.handlePrimColour}
                     // handleSecColor={this.}
                   />
                 ) : null}
