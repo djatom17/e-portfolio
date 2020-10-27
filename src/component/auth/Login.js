c641f27a047a90314c6af8c60cbf14465bb33d26
anjali <anjaliahuja99@yahoo.com>
2020-10-27 18:12:42 +1100

    change to antd

--- src/component/auth/Login.js
+++ src/component/auth/Login.js
@@ -5,7 +5,10 @@ import { login } from "../../actions/authActions";
 import { clearErrors } from "../../actions/errorActions";
 import { Redirect } from "react-router-dom";
 import GoogleButton from "./GoogleButton";
-import { Row } from "antd";
+import { Row, Form, Input, Button, Checkbox, Typography } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+
+const { Title } = Typography;
 
 class Login extends Component {
   state = {
@@ -55,14 +58,89 @@ class Login extends Component {
     const { isAuthenticated } = this.props;
     if (isAuthenticated) return <Redirect to="/my-profile" />;
 
+    const onFinish = (values) => {
+      const { email, password } = values;
+
+      const user = {
+        email,
+        password,
+      };
+      console.log("Received values of form: ", user);
+
+      // Attempt to login
+      this.props.login(user);
+    };
+
     return (
       <div className={"login body-height"}>
         <div className={"container"}>
           <div className={"row"}>
             <div className="col-mid-8 m-auto">
-              <h1 className="display-4 text-center">Login</h1>
+              <h1 className="display-4 text-center mt-5">Login</h1>
               <p className={"lead text-center"}>Log in and Start Flexing</p>
-              <form onSubmit={this.handleSubmit}>
+
+              <Form
+                name="normal_login"
+                className="login-form"
+                size="large"
+                initialValues={{
+                  remember: true,
+                }}
+                onFinish={onFinish}
+              >
+                <Form.Item
+                  name="email"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please enter your Email!",
+                    },
+                  ]}
+                >
+                  <Input
+                    prefix={<UserOutlined className="site-form-item-icon" />}
+                    placeholder="Email"
+                    onChange={this.handleChange("email")}
+                  />
+                </Form.Item>
+                <Form.Item
+                  name="password"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please enter your Password!",
+                    },
+                  ]}
+                >
+                  <Input
+                    prefix={<LockOutlined className="site-form-item-icon" />}
+                    type="password"
+                    placeholder="Password"
+                  />
+                </Form.Item>
+                <Form.Item>
+                  <Form.Item name="remember" valuePropName="checked" noStyle>
+                    <Checkbox>Remember me</Checkbox>
+                  </Form.Item>
+
+                  <a className="login-form-forgot" href="">
+                    Forgot password
+                  </a>
+                </Form.Item>
+
+                <Form.Item>
+                  <Button
+                    type="primary"
+                    htmlType="submit"
+                    className="login-form-button"
+                  >
+                    Log in
+                  </Button>
+                  Or <a href="">register now!</a>
+                </Form.Item>
+              </Form>
+
+              {/* <form onSubmit={this.handleSubmit}>
                 <div className="form-group">
                   <input
                     type={"email"}
@@ -84,7 +162,7 @@ class Login extends Component {
                   />
                 </div>
                 <input type="submit" className="btn btn-info btn-block mt-4" />
-              </form>
+              </form> */}
               <Row justify="center" className="mt-4">
                 <GoogleButton />
               </Row>
